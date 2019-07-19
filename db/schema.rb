3ff064627afaa767662f051ceff934825c4f7c41
lara <mood1021@gmail.com>
2019-07-19 13:17:13 +0800

    add test step 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_035536) do
+ActiveRecord::Schema.define(version: 2019_07_19_050832) do
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -7,16 +7,31 @@ RSpec.describe Cart, type: :model do
       cart.add_item 1
       expect(cart.empty?).to be false
     end
-  end
+  
 
-  it "如果加了相同種類的商品到購物車裡，購買項目(CartItem)並不會增加，但商品的數量會改變" do
-    cart = Cart.new                             
-    3.times { cart.add_item(1) }                
-    5.times { cart.add_item(2) }                
-    4.times { cart.add_item(3) }                
+    it "如果加了相同種類的商品到購物車裡，購買項目(CartItem)並不會增加，但商品的數量會改變" do
+      cart = Cart.new                             
+      3.times { cart.add_item(1) }                
+      5.times { cart.add_item(2) }                
+      4.times { cart.add_item(3) }                
 
-    expect(cart.items.length).to be 3           # 總共應該會有 3 個 item
-    expect(cart.items.first.quantity).to be 3   # 第 1 個 item 的數量會是 3
-    expect(cart.items.second.quantity).to be 5  # 第 2 個 item 的數量會是 5
+      expect(cart.items.length).to be 3           # 總共應該會有 3 個 item
+      expect(cart.items.first.quantity).to be 3   # 第 1 個 item 的數量會是 3
+      expect(cart.items.second.quantity).to be 5  # 第 2 個 item 的數量會是 5
+    end
+
+    it "商品可放置購物車內，也可以再拿出來" do
+      cart = Cart.new
+      p1 = Product.create(title:"guns gore")
+      p2 = Product.create(title:"steep")
+
+      3.times { cart.add_item(p1.id)}
+      2.times { cart.add_item(p2.id) }
+
+      expect(cart.items.first.product_id).to be p1.id
+      expect(cart.items.second.product_id).to be p2.id
+      expect(cart.items.first.product).to be_a Product
+    end
   end
 end
+  
